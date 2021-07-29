59e5edcfd5847976e1ca2c4a66a7c459b3ef017f
NahLima <naiarafx@gmail.com>
2021-07-29 09:21:01 -0300

    versão nova do exercício

--- hangman/hangman-v02.py
+++ hangman/hangman-v02.py
@@ -9,26 +9,32 @@ class Hangman:
     def __init__(self, word):
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
+        self.letters = set(word)
+        self.guessed_letters = []
         self.word = word
-        self.size = len(word)
-        self.masked_word = ['_'] * self.size
 
     def guess(self, char):
         if self.status != STATUS_ONGOING:
             raise ValueError("Game over")
-        if char not in self.word or char in self.masked_word:
+        if char not in self.letters or char in self.guessed_letters:
             self.remaining_guesses -= 1
         else:
-            for letter in range(0, self.size):
-                if char == self.word[letter]:
-                    self.masked_word[letter] = char
-        if self.get_masked_word() == self.word:
+            if char in self.letters:
+                self.guessed_letters.append(char)
+
+        if len(self.letters) == len(self.guessed_letters):
             self.status = STATUS_WIN
         if self.remaining_guesses < 0:
             self.status = STATUS_LOSE
 
-    def get_masked_word(self):
-        return "".join(self.masked_word)
-
     def get_status(self):
         return self.status
+
+    def get_masked_word(self):
+        masked_word = []
+        for letter in self.word:
+            if letter in self.guessed_letters:
+                masked_word.append(letter)
+            else:
+                masked_word.append("_")
+        return "".join(masked_word)
